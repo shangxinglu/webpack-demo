9431adc36daf140eed179870f4c7b7c3eef3c1a0
shangxinglu <2609209328@qq.com>
2021-07-13 08:01:16 +0800

    init

---
+++ basic/app.js
@@ -0,0 +1,5 @@
+import "./css.css";
+
+const el = document.createTextNode('哈哈哈哈哈');
+
+document.body.appendChild(el);
---
+++ basic/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    mode:'development',
+
+    entry:'./basic/app.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js',
+    },
+
+
+    module:{
+        rules:[
+            {test:/\.css$/,use:'css-loader'}
+        ]
+    }
+}
